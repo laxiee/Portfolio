ef48e102834aabfe1144d7af9e0aa021630bf777
Lakshya Saxena <lakshya.r.saxena@gmail.com>
2024-03-12 09:23:00 +0530

    added new timeline section

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      'purp': {
+        100: '#F4EEFF',
+        200: '#DCD6F7',
+        300: '#A6B1E1',
+        400: '#7b84a8',
+        500: '#0891b2',
+        600: '##434975',
+        700: '#1F2544',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
